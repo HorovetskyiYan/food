8d3cdfa55722b11a3a5de0c87aec2a7d3f0c7415
Horovetskyi-Yan <horovetskyiyan2000@gmail.com>
2021-10-31 14:38:14 +0300

    s

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   $('.main-slider').slick({
+      arrows:true,
+      dots:true,
+      adaptiveHeigh:true,
+      slidesToShow:4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+
+   });
+});
